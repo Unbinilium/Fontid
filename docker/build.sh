bc848b02520461d4cbddc5810ee8a363a6baf04c
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2020-08-16 22:40:00 +0800

    Automatically require root access

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Require root permission
+if [ "$(id -u)" != 0 ]; then
+    sudo bash "$0"
+    exit 1
+fi
+
 #Pushd to docker directory
 pushd "$(dirname $0)"
 
@@ -9,7 +15,7 @@ cp '../src/server.py' 'app/server.py'
 cp '../src/requirements.txt' 'requirements.txt'
 
 #Build docker image
-sudo docker build -t font-identifier-rest-api .
+docker build -t font-identifier-rest-api .
 
 #Cleanup
 rm -fr 'app' 'requirements.txt'
